8ff80243a1505bd7d0d81a6560684f1bab11628b
PawZet <pawelzurawski898@gmail.com>
2021-10-08 13:39:18 +0200

    commit_8

---
+++ src/components/code-editor.tsx
@@ -0,0 +1,5 @@
+import MonacoEditor from "@monaco-editor/react";
+
+export default function CodeEditor() {
+  return <MonacoEditor height="500px" />;
+}
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import * as esbuild from "esbuild-wasm";
 import { fetchPlugin } from "./plugins/fetch-plugin";
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
+import CodeEditor from "./components/code-editor";
 
 const App = () => {
   const ref = useRef<any>();
@@ -64,6 +65,7 @@ const App = () => {
 
   return (
     <div>
+      <CodeEditor />
       <textarea
         onChange={(e) => setInput(e.target.value)}
         value={input}
